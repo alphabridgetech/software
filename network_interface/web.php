a86afe6f633645388372342174dc6467fc3f4c52
alphabridgetech <alphabridgetech@gmail.com>
2026-04-24 17:14:21 +0530

    Update and rename network_interface/web.php to network_interfacenote day2

--- network_interface/web.php
+++
@@ -1,2 +0,0 @@
-Route::get('addhost/ip', [SystemBulkUploadController::class, 'addHostIp'])->name('addhost.ip');
-    Route::post('addhost/ip/save', [SystemBulkUploadController::class, 'addHostIpsave'])->name('addhost.ip.save');
